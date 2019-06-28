569ab93f76c055f357ef4e40d08765d0848ff900
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-28 23:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function produceDrivingRange(range) {
   return function(start, end){
-    const startNum = start.replace('th','').
+    const startNum = parseInt(start.replace('th',''));
+    const endNum = parseInt(end.replace('th',''));
+    const distance = Math.abs(startNum - endNum);
+
+    if (distance > range) {
+      return ""
+    } else {
+      return ""
+    }
   }
 }
